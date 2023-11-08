6536c1aa0e5e4cf5a77dc8fde17caa48249113e8
Ana Topalovic <anatopalovic00@gmail.com>
2023-11-08 18:33:10 +0100

    adding new tiles based on direction

--- My project/Assets/TempleRunPrefabs/TileSpawner.cs
+++ My project/Assets/TempleRunPrefabs/TileSpawner.cs
@@ -29,7 +29,8 @@ namespace TempleRun
         private List<GameObject> currentTiles;
         private List<GameObject> currentObstacles;
 
-        private void Start() {
+        private void Start()
+        {
             currentTiles = new List<GameObject>();
             currentObstacles = new List<GameObject>();
 
@@ -47,16 +48,53 @@ namespace TempleRun
         {
             Quaternion newTileRotation = tile.gameObject.transform.rotation * Quaternion.LookRotation(currentTileDirection, Vector3.up);
 
-            prevTile = GameObject.Instantiate(tile.gameObject, currentTileLocation, newTileRotation);
+            prevTile = Instantiate(tile.gameObject, currentTileLocation, newTileRotation);
             currentTiles.Add(prevTile);
-            currentTileLocation += Vector3.Scale(prevTile.GetComponent<Renderer>().bounds.size, currentTileDirection);
+
+            if (tile.type == TileType.STRAIGHT)
+            {
+                currentTileLocation += Vector3.Scale(prevTile.GetComponent<Renderer>().bounds.size, currentTileDirection);
+            }
         }
 
         private GameObject SelectRandomGameObjectFromList(List<GameObject> list)
         {
-            if(list.Count == 0) { return null; }
+            if (list.Count == 0) { return null; }
 
             return list[Random.Range(0, list.Count)];
+        }
+
+        public void AddNewDirection(Vector3 direction)
+        {
+            currentTileDirection = direction;
+            DeletePreviousTiles();
+
+            Vector3 tilePlacementScale;
+            if (prevTile.GetComponent<Tile>().type == TileType.SIDEWAYS)
+            {
+                tilePlacementScale = Vector3.Scale(prevTile.GetComponent<Renderer>().bounds.size / 2
+                 + (Vector3.one * startingTile.GetComponent<BoxCollider>().size.z / 2), currentTileDirection);
+            }
+            else
+            {
+                tilePlacementScale = Vector3.Scale((prevTile.GetComponent<Renderer>().bounds.size - (Vector3.one * 2))
+                 + (Vector3.one * startingTile.GetComponent<BoxCollider>().size.z / 2), currentTileDirection);
+            }
+
+            currentTileLocation += tilePlacementScale;
+
+            int currentPathLength = Random.Range(minimumStraightTiles, maximumStraightTiles);
+            for (int i = 0; i < currentPathLength; i++)
+            {
+                SpawnTile(startingTile.GetComponent<Tile>(), !(i == 0));
+            }
+
+            SpawnTile(SelectRandomGameObjectFromList(turnTiles).GetComponent<Tile>());
+        }
+
+        private void DeletePreviousTiles()
+        {
+
         }
     }
 }
